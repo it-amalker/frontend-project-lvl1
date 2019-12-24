b349f487d89c719cfa0976a6c83175180f863bd6
amalker <it.amalker@yandex.ru>
2019-12-24 18:41:34 +0100

    added some fixes due to eslint

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { greeting } from '..';
+import greeting from '..';
 
 console.log('Welcome to the Brain Games!\n');
 greeting();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
+const greeting = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
+
+export default greeting;
