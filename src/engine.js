adb3dd5df01c9fc4adafcdbfbfdd066a78ecb943
amalker <it.amalker@yandex.ru>
2019-12-28 12:11:00 +0100

    remove unnecessary functions, change engine arguments

--- src/engine.js
+++ src/engine.js
@@ -1,28 +1,24 @@
-import {
-  printGreeting, printGameRules, getUserName, getUserAnswer,
-} from '.';
+import readlineSync from 'readline-sync';
 
+const runGame = (rules, generateGameConditions) => {
+  console.log('Welcome to the Brain Games!');
+  console.log(rules);
 
-const runGame = (rules, generateTask, getCorrectAnswer) => {
-  printGreeting();
-  printGameRules(rules);
-
-  const userName = getUserName();
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
 
   const attempts = 3;
   for (let i = 0; i < attempts; i += 1) {
-    const task = generateTask();
-    console.log(`Question: ${task}`);
-    const correctAnswer = getCorrectAnswer(task);
-    const userAnswer = getUserAnswer();
+    const [taskConditions, correctAnswer] = generateGameConditions();
+    console.log(`Question: ${taskConditions}`);
+    const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
     } else {
-      return `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`;
+      return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
     }
   }
-  return `Congratulations, ${userName}!`;
+  return console.log(`Congratulations, ${userName}!`);
 };
 
 export default runGame;
