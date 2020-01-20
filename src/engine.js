1cdb6e65d8469465cb40b466d6f2d56d9e77c93a
amalker <it.amalker@yandex.ru>
2020-01-20 19:18:23 +0300

    remove greeting

--- src/engine.js
+++ src/engine.js
@@ -3,7 +3,6 @@ import readlineSync from 'readline-sync';
 const attemptsCount = 3;
 
 const runGame = (description, generateGameConditions) => {
-  console.log('Welcome to the Brain Games!');
   console.log(`${description}\n`);
 
   const userName = readlineSync.question('May I have your name? ');
