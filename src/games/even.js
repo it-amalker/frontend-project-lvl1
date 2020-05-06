0ca19a8fd8d4af7000d55118c7e86a06ed6abf3e
amalker <it.amalker@yandex.ru>
2020-05-06 12:28:22 +0300

    change default export

--- src/games/even.js
+++ src/games/even.js
@@ -3,12 +3,17 @@ import runGame from '../engine';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-export const isEven = (num) => num % 2 === 0;
+const isEven = (num) => num % 2 === 0;
 
 const generateGameConditions = () => {
   const question = getRandomNumber();
   const answer = isEven(question) ? 'yes' : 'no';
-  return [String(question), answer];
+  return [`${question}`, answer];
 };
 
-export default () => runGame(description, generateGameConditions);
+const exportFunctions = {
+  isEven,
+  run: () => runGame(description, generateGameConditions),
+};
+
+export default exportFunctions;
